4383043545571b9e26e1868dc65a10d21a1bfcd7
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-13 20:57:00 +0530

    bulk

---
+++ patna-metro/src/main/java/com/bihar/patna_metro/PatnaMetroApplication.java
@@ -0,0 +1,13 @@
+package com.bihar.patna_metro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatnaMetroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatnaMetroApplication.class, args);
+	}
+
+}
---
+++ patna-metro/src/test/java/com/bihar/patna_metro/PatnaMetroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bihar.patna_metro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatnaMetroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
