9f7c12f814e7c6b9862053a804264d53c559e376
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-14 04:50:15 +0530

    feat: Add FareCalculator utility to calculate metro fares based on distance slabs

--- patna-metro/src/main/java/com/bihar/patna_metro/controller/FareController.java
+++ patna-metro/src/main/java/com/bihar/patna_metro/controller/FareController.java
@@ -2,18 +2,22 @@ package com.bihar.patna_metro.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import com.bihar.patna_metro.service.FareService;
 
 @RestController
-@RequestMapping("/api/fare")
+@RequestMapping("/api")
 public class FareController {
 
     @Autowired
     private FareService fareService;
 
-    @GetMapping
+    @GetMapping("/fare")
     public double getFare(@RequestParam String source, @RequestParam String destination) {
         return fareService.calculateFare(source, destination);
     }
+//
+//    @GetMapping("/estimated-time")
+//    public double getEstimatedTime(@RequestParam String source, @RequestParam String destination) {
+//        return fareService.calculateEstimatedTime(source, destination);
+//    }
 }
