8398eb7b8ed703c3baeb3d600155586cc72f4179
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-15 22:41:12 +0530

    Exception

---
+++ Patna Metro_Frontend/src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NotFound() {
+  return (
+    <div className="text-center py-16">
+      <h1 className="text-3xl font-bold mb-4">404 - Page Not Found</h1>
+      <Link to="/" className="text-blue-600 underline">Go Home</Link>
+    </div>
+  );
+}
+
+export default NotFound;
