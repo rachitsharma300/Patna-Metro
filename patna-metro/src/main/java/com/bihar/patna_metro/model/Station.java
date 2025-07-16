09df058873ce79d7bc5240db985226729e014aea
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-16 06:25:30 +0530

    refactor: update Station model with sequenceNumber, latitude, and longitude fields

--- patna-metro/src/main/java/com/bihar/patna_metro/model/Station.java
+++ patna-metro/src/main/java/com/bihar/patna_metro/model/Station.java
@@ -25,7 +25,7 @@ public class Station {
         this.sequenceNumber = sequenceNumber;
     }
 
-    // Getters and Setters
+    // Getters and Setters { Not Need but Spring Auto Gen }
 
     public String getId() { return id; }
     public void setId(String id) { this.id = id; }
