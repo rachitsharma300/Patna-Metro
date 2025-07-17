8bc38897479cd76b86095d9ae378872d18f5a7c9
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-17 17:18:08 +0530

    feat(layout): restructure App component layout and add new routes

--- Patna Metro_Frontend/src/App.jsx
+++ Patna Metro_Frontend/src/App.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
+// src/App.jsx
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import About from './pages/About'
+import FareInfo from './pages/FareInfo'
+import MapPage from './pages/MapPage'
+import NotFound from './pages/NotFound'
+
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Footer />
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/fare-info" element={<FareInfo />} />
+            <Route path="/metro-map" element={<MapPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
