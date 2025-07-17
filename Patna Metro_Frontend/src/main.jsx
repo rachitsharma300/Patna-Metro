343d1ae52da9c607c0d0e2e6f64807d31fe2249c
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-17 17:18:13 +0530

    feat(context): wrap App component with LanguageProvider for internationalization support

--- Patna Metro_Frontend/src/main.jsx
+++ Patna Metro_Frontend/src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { LanguageProvider } from './utils/LanguageContext';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
   </React.StrictMode>
-);
+)
