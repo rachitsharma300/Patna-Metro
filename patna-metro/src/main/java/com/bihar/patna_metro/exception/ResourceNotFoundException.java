d7333e90707feaebdb2e5221e75544a05459d1f0
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-22 05:35:46 +0530

    feat: create ResourceNotFoundException for handling missing resource scenarios

---
+++ patna-metro/src/main/java/com/bihar/patna_metro/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bihar.patna_metro.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
