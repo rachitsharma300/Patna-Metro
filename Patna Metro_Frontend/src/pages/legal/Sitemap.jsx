a5123aac8dcb444d0200a7ae78233f9bfc4966d7
rachitsharma300 <rachitsharma300@gmail.com>
2025-07-22 21:31:59 +0530

    feat: generate Sitemap page listing all routes and pages

---
+++ Patna Metro_Frontend/src/pages/legal/Sitemap.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Sitemap() {
+  return (
+    <div className="container mx-auto px-4 py-20">
+      <h1 className="text-3xl font-bold mb-6">Sitemap</h1>
+      <ul className="list-disc pl-6">
+        <li>Home</li>
+        <li>Route Finder</li>
+        <li>Fare Info</li>
+        <li>Metro Map</li>
+        <li>About</li>
+        <li>Privacy Policy</li>
+        <li>Terms of Service</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sitemap;
