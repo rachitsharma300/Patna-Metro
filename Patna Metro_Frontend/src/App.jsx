8110138ca11f1e94191b6fc56b0557354a2fb9c3
rachitsharma300 <rachitsharma300@gmail.com>
2025-08-03 16:34:49 +0530

    feat: Bot map

--- Patna Metro_Frontend/src/App.jsx
+++ Patna Metro_Frontend/src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import React from "react";
 import "./i18n";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -14,13 +13,14 @@ import PrivacyPolicy from "./pages/legal/PrivacyPolicy";
 import TermsOfService from "./pages/legal/TermsOfService";
 import Sitemap from "./pages/legal/Sitemap";
 import DisclaimerPopup from "./components/DisclaimerPopup";
+import Bot from "./components/bot/Bot";
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
         <Navbar />
-        
+
         {/* DisclaimerPopup */}
         <DisclaimerPopup />
 
@@ -32,12 +32,15 @@ function App() {
             <Route path="/fare-info" element={<FareInfo />} />
             <Route path="/metro-map" element={<MapPage />} />
             <Route path="*" element={<NotFound />} />
-
             <Route path="/privacy-policy" element={<PrivacyPolicy />} />
             <Route path="/terms-of-service" element={<TermsOfService />} />
             <Route path="/sitemap" element={<Sitemap />} />
           </Routes>
         </main>
+
+        {/* Place Bot here, outside Routes */}
+        <Bot />
+
         <Footer />
       </div>
     </Router>
