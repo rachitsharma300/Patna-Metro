413aa137a426e483a6e494173a3829e35605e323
rachitsharma300 <rachitsharma300@gmail.com>
2025-08-05 23:05:56 +0530

    feat: Implement Bot - Bodhi on backend Config file for Bot only.

---
+++ patna-metro/src/main/java/com/bihar/patna_metro/bot/config/BotConfig.java
@@ -0,0 +1,8 @@
+package com.bihar.patna_metro.bot.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BotConfig {
+    //  For Future TTS/Speech Recognition config
+}
