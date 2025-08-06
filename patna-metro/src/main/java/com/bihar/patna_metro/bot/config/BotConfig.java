2f390ec94ec907e59a94a0d73f6ce1b40acd6c4f
rachitsharma300 <rachitsharma300@gmail.com>
2025-08-06 23:02:29 +0530

    feat: Bot Config for future Implementation

--- patna-metro/src/main/java/com/bihar/patna_metro/bot/config/BotConfig.java
+++
@@ -1,8 +0,0 @@
-package com.bihar.patna_metro.bot.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class BotConfig {
-    //  For Future TTS/Speech Recognition config
-}
