820be6137c7f6c1b97a40359617de57472a78c80
rachitsharma300 <rachitsharma300@gmail.com>
2025-08-06 23:03:20 +0530

    feat: handle bol response

--- patna-metro/src/main/java/com/bihar/patna_metro/bot/dto/BotResponse.java
+++ patna-metro/src/main/java/com/bihar/patna_metro/bot/dto/BotResponse.java
@@ -1,13 +1,24 @@
 package com.bihar.patna_metro.bot.dto;
 
-import lombok.Builder;
-import lombok.Data;
+import java.util.List;
+import com.bihar.patna_metro.model.Station;
 
-@Data
-@Builder
 public class BotResponse {
-    private String hindiText;    // For Hindi Response
-    private int timeMinutes;     // Travel time in min
-    private int fareRupees;      // Fare
-    private String interchange;  // If exists interchange
+    private String textResponse; // बोट का जवाब (हिंदी/इंग्लिश)
+    private List<Station> route;
+    private int fare;
+    private int timeMinutes;
+    private String interchange;
+
+    // Getters & Setters
+    public String getTextResponse() { return textResponse; }
+    public void setTextResponse(String textResponse) { this.textResponse = textResponse; }
+    public List<Station> getRoute() { return route; }
+    public void setRoute(List<Station> route) { this.route = route; }
+    public int getFare() { return fare; }
+    public void setFare(int fare) { this.fare = fare; }
+    public int getTimeMinutes() { return timeMinutes; }
+    public void setTimeMinutes(int timeMinutes) { this.timeMinutes = timeMinutes; }
+    public String getInterchange() { return interchange; }
+    public void setInterchange(String interchange) { this.interchange = interchange; }
 }
