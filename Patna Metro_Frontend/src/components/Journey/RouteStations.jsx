56773f0a7c86cc2ac0c644f115c1a4911e3f12f6
rachitsharma300 <rachitsharma300@gmail.com>
2025-08-07 23:32:30 +0530

    fix: motion warning - /* eslint-disable no-unused-vars */

--- Patna Metro_Frontend/src/components/Journey/RouteStations.jsx
+++ Patna Metro_Frontend/src/components/Journey/RouteStations.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+/* eslint-disable no-unused-vars */
 import { motion } from "framer-motion";
 import StationCard from "../StationCard";
 import StationTrack from "../StationTrack";
