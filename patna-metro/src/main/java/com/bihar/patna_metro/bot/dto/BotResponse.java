e5720d215c522ab0de53acd5211de2a6b1f8986f
rachitsharma300 <rachitsharma300@gmail.com>
2025-08-10 19:45:31 +0530

    rm: Not needed right now may be in future

--- patna-metro/src/main/java/com/bihar/patna_metro/bot/dto/BotResponse.java
+++ patna-metro/src/main/java/com/bihar/patna_metro/bot/dto/BotResponse.java
@@ -1,24 +1,26 @@
-package com.bihar.patna_metro.bot.dto;
+//package com.bihar.patna_metro.bot.dto;
+//
+//import java.util.List;
+//import com.bihar.patna_metro.model.Station;
+//
+//public class BotResponse {
+//    private String textResponse; // बोट का जवाब (हिंदी/इंग्लिश)
+//    private List<Station> route;
+//    private int fare;
+//    private int timeMinutes;
+//    private String interchange;
+//
+//    // Getters & Setters
+//    public String getTextResponse() { return textResponse; }
+//    public void setTextResponse(String textResponse) { this.textResponse = textResponse; }
+//    public List<Station> getRoute() { return route; }
+//    public void setRoute(List<Station> route) { this.route = route; }
+//    public int getFare() { return fare; }
+//    public void setFare(int fare) { this.fare = fare; }
+//    public int getTimeMinutes() { return timeMinutes; }
+//    public void setTimeMinutes(int timeMinutes) { this.timeMinutes = timeMinutes; }
+//    public String getInterchange() { return interchange; }
+//    public void setInterchange(String interchange) { this.interchange = interchange; }
+//}
 
-import java.util.List;
-import com.bihar.patna_metro.model.Station;
-
-public class BotResponse {
-    private String textResponse; // बोट का जवाब (हिंदी/इंग्लिश)
-    private List<Station> route;
-    private int fare;
-    private int timeMinutes;
-    private String interchange;
-
-    // Getters & Setters
-    public String getTextResponse() { return textResponse; }
-    public void setTextResponse(String textResponse) { this.textResponse = textResponse; }
-    public List<Station> getRoute() { return route; }
-    public void setRoute(List<Station> route) { this.route = route; }
-    public int getFare() { return fare; }
-    public void setFare(int fare) { this.fare = fare; }
-    public int getTimeMinutes() { return timeMinutes; }
-    public void setTimeMinutes(int timeMinutes) { this.timeMinutes = timeMinutes; }
-    public String getInterchange() { return interchange; }
-    public void setInterchange(String interchange) { this.interchange = interchange; }
-}
+//Not Needed
