b354aade9692a00bf95b7871078c2ca221eeb570
rachitsharma300 <rachitsharma300@gmail.com>
2025-08-10 19:45:13 +0530

    rm: Not needed riht now may be in future

--- patna-metro/src/main/java/com/bihar/patna_metro/bot/dto/BotRequest.java
+++ patna-metro/src/main/java/com/bihar/patna_metro/bot/dto/BotRequest.java
@@ -1,12 +1,14 @@
-package com.bihar.patna_metro.bot.dto;
+//package com.bihar.patna_metro.bot.dto;
+//
+//public class BotRequest {
+//    private String input; // वॉइस/टेक्स्ट इनपुट
+//    private boolean isVoice; // true = वॉइस, false = टेक्स्ट
+//
+//    // Getters & Setters
+//    public String getInput() { return input; }
+//    public void setInput(String input) { this.input = input; }
+//    public boolean isVoice() { return isVoice; }
+//    public void setVoice(boolean voice) { isVoice = voice; }
+//}
 
-public class BotRequest {
-    private String input; // वॉइस/टेक्स्ट इनपुट
-    private boolean isVoice; // true = वॉइस, false = टेक्स्ट
-
-    // Getters & Setters
-    public String getInput() { return input; }
-    public void setInput(String input) { this.input = input; }
-    public boolean isVoice() { return isVoice; }
-    public void setVoice(boolean voice) { isVoice = voice; }
-}
+// Not Needed
