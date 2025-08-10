5f129bc1e04feb5afc62f02400a147a25666d8c6
rachitsharma300 <rachitsharma300@gmail.com>
2025-08-10 19:45:39 +0530

    rm: Not needed right now may be in future

--- patna-metro/src/main/java/com/bihar/patna_metro/bot/dto/RouteDetails.java
+++ patna-metro/src/main/java/com/bihar/patna_metro/bot/dto/RouteDetails.java
@@ -1,14 +1,16 @@
-package com.bihar.patna_metro.bot.dto;
+//package com.bihar.patna_metro.bot.dto;
+//
+//import com.bihar.patna_metro.model.Station;
+//import lombok.Data;
+//import java.util.List;
+//
+//@Data
+//public class RouteDetails {
+//    private List<String> lines;
+//    private int totalTime;
+//    private int fare;
+//    private String interchange;
+//    private List<Station> stations;
+//}
 
-import com.bihar.patna_metro.model.Station;
-import lombok.Data;
-import java.util.List;
-
-@Data
-public class RouteDetails {
-    private List<String> lines;
-    private int totalTime;
-    private int fare;
-    private String interchange;
-    private List<Station> stations;
-}
+//Not Needed
