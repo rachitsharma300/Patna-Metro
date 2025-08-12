7658c9374a183e7807227ab7890b2e2572340304
rachitsharma300 <rachitsharma300@gmail.com>
2025-08-12 22:30:59 +0530

    fix: for deployment

--- patna-metro/src/test/java/com/bihar/patna_metro/PatnaMetroApplicationTests.java
+++ patna-metro/src/test/java/com/bihar/patna_metro/PatnaMetroApplicationTests.java
@@ -2,12 +2,13 @@ package com.bihar.patna_metro;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // Ye line ADD KARO
 class PatnaMetroApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Test pass hoga agar Spring context load ho jaye
+    }
 }
