b3af13bb8134650786ecec42293fc8493a57495b
rachitsharma300 <rachitsharma300@gmail.com>
2025-08-20 18:24:59 +0530

    fix: migirate AWS into Render

--- Patna Metro_Frontend/vite.config.js
+++ Patna Metro_Frontend/vite.config.js
@@ -1,3 +1,4 @@
+// ORIGIANL VITE
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // import svgr from 'vite-plugin-svgr'; 
@@ -10,3 +11,25 @@ export default defineConfig({
     port: 5173,
   },
 }); 
+
+
+// AWS PROXY ROUTE HTTP TO HANDLE BROWSER
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0',
+//     port: 5173,
+//     proxy: {
+//       '/api': {
+//         target: 'http://patna-metro-b.ap-south-1.elasticbeanstalk.com',
+//         changeOrigin: true,
+//         secure: false, // Required for proxying to HTTP target
+//         rewrite: (path) => path.replace(/^\/api/, '')
+//       }
+//     }
+//   },
+// });
