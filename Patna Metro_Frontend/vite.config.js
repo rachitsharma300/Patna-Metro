1040e9bda4234af4fcea3f3d182a1e25bb930971
rachitsharma300 <rachitsharma300@gmail.com>
2026-03-08 00:55:32 +0530

    feat: Implement the initial Patna Metro frontend application, including core pages, components, services, and admin functionalities.

--- Patna Metro_Frontend/vite.config.js
+++ Patna Metro_Frontend/vite.config.js
@@ -1,11 +1,12 @@
-// ORIGIANL VITE
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import svgr from 'vite-plugin-svgr'; 
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
   server: {
     host: '0.0.0.0',
     port: 5173,
